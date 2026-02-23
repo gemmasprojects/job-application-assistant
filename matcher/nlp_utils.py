c7c9ca47010c62cd4d149f1ecb36ac324ba8d3ed
gemmasprojects <gemmamgm92@gmail.com>
2026-02-23 16:57:50 +0000

    Create nlp_utils.py

---
+++ matcher/nlp_utils.py
@@ -0,0 +1,7 @@
+import re
+
+def extract_keywords(text):
+    text = text.lower()
+    text = re.sub(r"[^a-z0-9\s]", " ", text)
+    tokens = [t for t in text.split() if len(t) > 2]
+    return set(tokens)
